9cf946b1befd6a791c6bb64f2254e38fc080d3bb
lizarragalucasmaximiliano <lizarragalucasmaximiliano@gmail.com>
2022-08-08 13:54:31 +0200

    tarea

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,9 +60,9 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
- if ( x == y ) {
+ if ( x === y ) {
   return true;
- }
+ } else {
   return false;
 }
 
@@ -70,7 +70,10 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if ( str1 === str2) {
+    return true;
+  } else {
+    return false;
 }
 
 function menosQueNoventa(num) {

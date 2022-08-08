d47105a38d83d795e826a68486b872eebbf0491d
lizarragalucasmaximiliano <lizarragalucasmaximiliano@gmail.com>
2022-08-08 13:57:49 +0200

    tarea232

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -70,7 +70,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ( str1 === str2) {
+  if ( str1 === str2 ) {
     return true;
   } else {
     return false;
